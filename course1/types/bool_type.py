77fc6b177b310b3bbc922b36186be6bb0aff1043
sobolevn <mail@sobolevn.me>
2017-01-25 13:06:47 +0300

    Lecture 1

---
+++ course1/types/bool_type.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# True
+print(True)
+
+# False
+print(False)
+
+
+# Comparing result
+print('4 > 3 =', 4 > 3)
+print('4 > 5 =', 4 > 5)
+
+# Boolean logic
+print('True and False', True and False)
+print('True or False', True or False)
